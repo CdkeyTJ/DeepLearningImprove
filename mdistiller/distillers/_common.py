9a5fbb63581ecceca3126fe0bc51c8c394427769
wx <cdk0622@163.com>
2025-06-06 11:14:59 +0800

    modified:   mdistiller/distillers/_common.py 	modified:   mdistiller/engine/__pycache__/trainer.cpython-38.pyc

--- mdistiller/distillers/_common.py
+++ mdistiller/distillers/_common.py
@@ -9,6 +9,7 @@ class ConvReg(nn.Module):
     def __init__(self, s_shape, t_shape, use_relu=True, use_bn=True):
         super(ConvReg, self).__init__()
         self.use_relu = use_relu
+        # 决定是否在前向传播中使用Batch normalization
         self.use_bn = use_bn
         s_N, s_C, s_H, s_W = s_shape
         t_N, t_C, t_H, t_W = t_shape
@@ -20,6 +21,7 @@ class ConvReg(nn.Module):
             self.conv = nn.Conv2d(s_C, t_C, kernel_size=(1 + s_H - t_H, 1 + s_W - t_W))
         else:
             raise NotImplemented("student size {}, teacher size {}".format(s_H, t_H))
+        
         self.bn = nn.BatchNorm2d(t_C)
         self.relu = nn.ReLU(inplace=True)
 
@@ -27,6 +29,7 @@ class ConvReg(nn.Module):
         x = self.conv(x)
         if self.use_relu:
             return self.relu(self.bn(x))
+        # 判断是否需要bn（这看起来无用）
         elif self.use_bn:
             return self.bn(x)
         else:
@@ -42,7 +45,7 @@ def get_feat_shapes(student, teacher, input_size):
     feat_t_shapes = [f.shape for f in feat_t["feats"]]
     return feat_s_shapes, feat_t_shapes
 
-# by CDK
+# by CDK  服务于WKD方法
 class ConvRegBottleNeck(nn.Module):
     def __init__(self, s_shape, t_shape, c_hidden, use_relu=True, use_bn=True):
         super(ConvRegBottleNeck, self).__init__()
